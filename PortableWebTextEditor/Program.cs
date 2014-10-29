2fd409af8361799a64f7cdd71d51136051b702ba
Josh Schlesinger <josh.schlesinger@market6.com>
2014-10-29 17:53:47 -0500

    initial commit with ace files and a basic editor

---
+++ PortableWebTextEditor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace PortableWebTextEditor
+{
+    using System;
+    using Nancy.Hosting.Self;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri =
+                new Uri("http://localhost:3579");
+
+            using (var host = new NancyHost(uri))
+            {
+                
+                host.Start();
+
+                Process.Start(uri.AbsoluteUri);
+                Console.WriteLine("Your application is running on " + uri);
+                Console.WriteLine("Press any [Enter] to close the host.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
